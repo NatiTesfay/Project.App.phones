2026ca959ac27e4266115afbc1260f68ab3d4f9e
NatiTesfay <netaneltes66@gmail.com>
2022-07-05 19:02:00 +0300

    1

--- main.js
+++ main.js
@@ -30,13 +30,41 @@ function printApi() {
 }
 
 
+
 async function objectsLoops() {
+    let holder = document.getElementById("holderHolder");
     let array = await getApi()
-  array.forEach(element => {
-    console.log(element)
+  array.forEach(device => {
+    holder.innerHTML += `<div id="${device.id}" class="card">
+        <div>
+            <p>id:${device.id}</p>
+            <p>price:${device.price}</p> 
+            <p>isAvilabale:${device.isAvailable}</p> 
+            <p>createAt:${device.createdAt}</p> 
+            <p>color:${device.color}</p>
+            <p>brand:${device.brand}</p>
+            <p>ram:${device.ram}</p>
+        </div>
+    </div>
+    `
   });
 }
 objectsLoops();
+
+// function objectsHolder(device) {
+   
+//   let text1 =  `<div class = holderHolder>
+//     <p>id:${device.id}</p>
+//     <p>price:${device.price}</p> 
+//     <p>isAvilabale:${device.isAvailable}</p> 
+//     <p>createAt:${device.createdAt}</p> 
+//     <p>color:${device.color}</p>
+//     <p>brand:${device.brand}</p>
+//     <p>ram:${device.ram}</p>
+//      </div>
+//     `
+//     return text1
+// }
 
 
 
